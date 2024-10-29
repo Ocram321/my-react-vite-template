9ce53b4134bdb71f1198848e3dad0028dd9a1c63
Marco Milinovich <ocram321@gmail.com>
2024-10-29 15:00:04 -0700

    React vite template

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// vite.config.js
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({ // <-- Change this line too!
+  plugins: [
+    react(),
+    eslint({
+      lintOnStart: true,
+      failOnError: mode === "production"
+    })
+  ]
+}))
